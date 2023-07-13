bd7e2d53d7ebcbc21c569a610f8e23252503a3e7
ikeali <ikechukwualisigwe01@gmail.com>
2023-07-13 07:06:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,16 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints "Programming is like building a multilingual puzzle"
- *              followed by a new line using the puts function.
+ * Description:
+ *   Prints "Programming is like building a multilingual puzzle"
+ *   followed by a new line using the puts function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
 }
 
