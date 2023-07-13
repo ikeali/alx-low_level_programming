27990aff9680fb5be671ae1d273df1e8cf4e8f68
ikeali <ikechukwualisigwe01@gmail.com>
2023-07-13 09:19:12 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,6 @@
 /**
  * main - Entry point of the program
  *
- * Description:
- *   Prints "Programming is like building a multilingual puzzle"
- *   followed by a new line using the puts function.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
