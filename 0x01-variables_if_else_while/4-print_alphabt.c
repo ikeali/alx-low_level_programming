314e38b95f055bed6fdeaa91517ee7710fe64133
ikeali <ikechukwualisigwe01@gmail.com>
2023-07-14 12:25:25 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,11 @@
 int main(void)
 {
 	char low, e, q;
-
 	e = 'e';
 	q = 'q';
-	
 	for (low = 'a'; low <= 'z'; low++)
 	{
-		if (low != e && low !=q)
+		if (low != e && low != q)
 			putchar(low);
 	}
 	putchar('\n');
