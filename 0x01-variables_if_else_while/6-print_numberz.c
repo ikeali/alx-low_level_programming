501066bfaaee6aa3f0508d772af16878431a87ed
ikeali <ikechukwualisigwe01@gmail.com>
2023-07-14 13:13:21 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,10 +8,10 @@
 */
 int main(void)
 {
-        int d;
+	int d;
 
-        for (d = '0'; d <= '9'; d++)
-                putchar(d);
-        putchar('\n');
-        return (0);
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
 }
