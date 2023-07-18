2b0936a36c4a77cb955d2469367bc037102dc085
ikeali <ikechukwualisigwe01@gmail.com>
2023-07-18 06:36:31 -0700

    all

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,7 +1,7 @@
 #include<unistd.h>
 /**
- * prints _putchar
- *
+ * _putchar - writes the character c to studout
+ * @c: The  character to print
  * Return: On Success 1
  * On error -1 is returned, and error is set appropriately
  */
