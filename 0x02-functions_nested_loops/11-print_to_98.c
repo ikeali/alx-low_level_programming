e27dfb6815f4309a6117b73ba247cff9779d9700
ikeali <ikechukwualisigwe01@gmail.com>
2023-07-19 10:56:26 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,13 @@
  */
 void print_to_98(int n)
 {
-	if (n <=98)
+	if (n <= 98)
 	{
 		for (; n <= 98; n++)
 		{
 			if (n == 98)
 			{
-				printf("%d",n);
+				printf("%d", n);
 					printf("\n");
 				break;
 			}
@@ -30,7 +30,7 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d",n);
+				printf("%d", n);
 					printf("\n");
 				break;
 			}
