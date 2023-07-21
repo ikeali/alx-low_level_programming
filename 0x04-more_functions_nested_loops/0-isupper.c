2b977175caf9376535ae813ed9fbaabcaffe4d0a
ikeali <ikechukwualisigwe01@gmail.com>
2023-07-21 06:42:50 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Uppercase letters
+ * @c: char to check
+ *
+ * Return:return 1 or 0
+ */
+int _isupper (int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
