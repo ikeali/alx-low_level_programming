29c078abe5d4c225744270ccad0bee0ad912cc3d
ikeali <ikechukwualisigwe01@gmail.com>
2023-07-31 07:09:52 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putcha - writes the character c to stdout
+ * @c: the chracter to print
+ * Return: On success 1
+ * On error,  -1  is returned and erno is set appropraitely
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
