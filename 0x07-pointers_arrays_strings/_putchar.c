4e863232213b4c601cbc55ea3eaa4438ca70ac8c
ikeali <ikechukwualisigwe01@gmail.com>
2023-08-01 10:33:42 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <unistd.h>
 /**
  * _putcha - writes the character c to stdout
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <string.h>
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
